5e07ab2cf2e3696760618d6320c9432941d6e5e0
Jorge A. Romero <ja.romerog12@uniandes.edu.co>
2024-03-08 22:08:54 -0500

    navegación aparentemente ok

---
+++ app/src/main/java/com/example/relaxreminder/ui/DashboardActivity.kt
@@ -0,0 +1,87 @@
+package com.example.relaxreminder.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.example.relaxreminder.R
+import com.example.relaxreminder.databinding.ActivityDashboardBinding
+import com.google.android.material.navigation.NavigationView
+
+class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityDashboardBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        toggle = ActionBarDrawerToggle(
+            this, drawerLayout,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        if(savedInstanceState == null) {
+            loadDefaultFragment()
+        }
+    }
+
+    private fun loadDefaultFragment() {
+        val item: MenuItem =  navigationView.menu.getItem(1)
+        onNavigationItemSelected(item);
+    }
+
+    private fun goToHome() {
+        nextActivity(
+            DashboardActivity::class.java,
+            Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+        )
+    }
+
+    private fun changeFragment(fragment: Fragment, title: String) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+    }
+
+    private fun nextActivity(activity: Class<*>, flags: Int? = null, extras: List<Pair<String, String>>? = null) {
+        val intent = Intent(this, activity)
+        flags?.let {
+            intent.addFlags(flags)
+        }
+
+        extras?.let{
+            for ((key, value) in extras) {
+                println("$key = $value")
+                intent.putExtra(key, value)
+            }
+        }
+
+        startActivity(intent)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        item.isChecked = true
+        when(item.itemId) {
+            R.id.nav_item_home -> goToHome()
+            //R.id.nav_item_routine -> changeFragment(VisitorHomeFragment.newInstance(), item.title.toString())
+            }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
--- app/src/main/java/com/example/relaxreminder/ui/LoginActivity.kt
+++ app/src/main/java/com/example/relaxreminder/ui/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.example.relaxreminder.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.relaxreminder.R
+import com.example.relaxreminder.databinding.ActivityLoginBinding
+import com.google.android.material.button.MaterialButton
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLogIn.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
