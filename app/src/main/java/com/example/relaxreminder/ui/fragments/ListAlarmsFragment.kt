bee67ad6b8e3eb0cfe9a31af85a0fec9330e7e19
Jorge A. Romero <ja.romerog12@uniandes.edu.co>
2024-03-09 11:57:41 -0500

    faltan iconos de editar y eliminar

--- app/src/main/java/com/example/relaxreminder/ui/fragments/ListAlarmsFragment.kt
+++ app/src/main/java/com/example/relaxreminder/ui/fragments/ListAlarmsFragment.kt
@@ -5,21 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.relaxreminder.R
 import com.example.relaxreminder.databinding.FragmentListAlarmsBinding
+import com.example.relaxreminder.model.Alarm
+import com.example.relaxreminder.ui.adapter.AlarmsAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ListAlarmsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListAlarmsFragment : Fragment() {
+
+    private var alarms = listOf(
+        Alarm(
+            name = "Caminar",
+            start = "50",
+            end = "20",
+            id = "1"
+        )
+    )
+
 
     private lateinit var binding: FragmentListAlarmsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,28 +36,17 @@ class ListAlarmsFragment : Fragment() {
     ): View {
         binding = FragmentListAlarmsBinding.inflate(inflater, container, false)
 
-        val recyclerView: RecyclerView = binding.r
+        val recyclerView: RecyclerView = binding.recyclerViewAlarms
+        val alarmAdapter = AlarmsAdapter(this.alarms)
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = alarmAdapter
 
         return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListAlarmsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListAlarmsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = ListAlarmsFragment()
     }
 }
